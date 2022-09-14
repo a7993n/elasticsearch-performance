9ca001bad1eb0a3ca6a1d6606556645115c055be
ajarouih <contact@ajarouih.me>
2022-09-14 12:05:43 +0100

    release@1.0

---
+++ README.md
@@ -0,0 +1,24 @@
+# elasticsearch-optimizer
+Make ES faster than ever! Check that your elasticsearch cluster is ready for production
+
+# Installation
+```bash
+npm i -g elasticsearch-optimizer
+```
+
+# Usage
+```bash
+es-optimizer my-awesome-cluster 9200
+```
+
+# Screenshot
+
+![](https://raw.githubusercontent.com/Goyoo/elasticsearch-optimizer/master/img/intro.jpg)
+
+# Contribute
+Please suggestions good improvements, blog posts and existing scripts [there](https://github.com/Goyoo/elasticsearch-optimizer/issues/1).
+Don't be shy ! We will put you in the contributors list :bowtie:
+
+# Contributors
+
+* [@Doweig](https://github.com/Doweig)
---
+++ index.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const program = require('commander');
+const validator = require('validator');
+const helper = require('./lib/helper');
+ 
+program
+    .version('0.0.1')
+    .usage('[options] <host> <port>')
+    .parse(process.argv)
+;
+
+/*
+ * Host and port sanitizing
+ */
+let host = '127.0.0.1';
+if (program.args[0]) {
+    // if (!validator.isIP(program.args[0]))
+    //     return console.error('<host> should be a valid IP address.')
+    host = program.args[0];
+}
+
+let port = 9200;
+if (program.args[1]) {
+    if (!validator.isInt(program.args[1], { min: 0, max: 0xFFFF })) {
+        return console.error('<port> should be a valid port number.');
+    }
+    
+    port = program.args[1];
+}
+
+global.ES_HOST = host;
+global.ES_PORT = port;
+
+/*
+ * ES version support checks
+ */
+helper.getESVersion((error, version) => {
+    if (error) return helper.failAndExit(error);
+
+    if (2 != version.major) {
+        return helper.failAndExit(new Error('This module only supports ES 2.x.'));
+    }
+
+    require('./lib/2_x').start(version);
+});
